2aaa941c65e2203c4e75fc1a617dd4ec9760edac
Alexandru Matei <alphaindiamike@users.noreply.github.com>
2023-11-22 06:32:22 +0100

    Front-end initial version, updated gitignore for fe

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,61 @@
+import { Wrap, WrapItem, Spinner, Text } from '@chakra-ui/react'
+import SidebarWithHeader from './components/shared/Sidebar';
+import { useEffect, useState } from 'react';
+import { getCustomers } from './services/client';
+import CardWithImage from './components/CardWithImage';
+
+const App = () => {
+
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function() {
+    setLoading(true);
+    setTimeout( () => {
+      getCustomers().then(res => {
+        //console.log(res);
+        setCustomers(res.data);
+      }).catch(err => {
+        console.log(err);
+      }).finally(() => {
+        setLoading(false);
+      })
+    }, 500)
+  }, [])
+
+  if (loading) {
+    return (
+    <SidebarWithHeader>
+      <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='blue.500'
+        size='xl'></Spinner>
+    </SidebarWithHeader>)
+  }
+
+  if (customers.length <= 0){
+    return (
+      <SidebarWithHeader>
+        <Text>Empty</Text>
+      </SidebarWithHeader>
+    )
+  }
+
+  return (
+    <SidebarWithHeader>
+      <Wrap justify={"left"} spacing={"30px"}>
+        {customers.map((customer, index) => (
+          <WrapItem key={index}>
+            <CardWithImage
+              {...customer}
+            ></CardWithImage>
+          </WrapItem>
+        ))}
+      </Wrap>
+    </SidebarWithHeader>
+  )
+}
+
+export default App;
---
+++ frontend/react/src/components/shared/SidebarContent.jsx
@@ -0,0 +1,64 @@
+import {
+    Box,
+    CloseButton,
+    Flex,
+    useColorModeValue,
+    Text,
+    Image,
+  } from '@chakra-ui/react'
+  import{
+    FiHome,
+    FiTrendingUp,
+    FiCompass,
+    FiStar,
+    FiSettings,
+  } from 'react-icons/fi'
+  import logo from '../../assets/logo-q.png';
+  import NavItem from './NavItem';
+
+const LinkItems = [
+    { name: 'Home', icon: FiHome },
+    { name: 'Trending', icon: FiTrendingUp },
+    { name: 'Explore', icon: FiCompass },
+    { name: 'Favourites', icon: FiStar },
+    { name: 'Settings', icon: FiSettings },
+  ]
+
+const SidebarContent = ({ onClose, ...rest }) => {
+    return (
+      <Box
+        transition="3s ease"
+        bg={useColorModeValue('white', 'gray.900')}
+        borderRight="1px"
+        borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+        w={{ base: 'full', md: 60 }}
+        pos="fixed"
+        h="full"
+        {...rest}>
+        <Flex h="20" alignItems="center" mx="8"
+            mb={75} mt={2} justifyContent="space-between">
+            <Flex h="20" flexDirection="column" alignItems="center" mx="8"
+                justifyContent="space-between">
+            <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold"
+                mb={5}>
+                Dashboard
+            </Text>
+            <Image
+                borderRadius='full'
+                boxSize='75px'
+                src={logo}
+                alt='q-logo'
+                />
+            </Flex>
+            <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        </Flex>
+        {LinkItems.map((link) => (
+          <NavItem key={link.name} icon={link.icon}>
+            {link.name}
+          </NavItem>
+        ))}
+      </Box>
+    )
+  }
+
+  export default SidebarContent
