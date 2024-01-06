5b96184e21c0e54e5dee18530169044f97713b51
Alexandru Matei <alphaindiamike@users.noreply.github.com>
2024-01-06 17:49:49 +0100

    File upload with tests - backend

--- backend/src/test/java/com/stag22/journey/CustomerIntegrationTest.java
+++ backend/src/test/java/com/stag22/journey/CustomerIntegrationTest.java
@@ -3,7 +3,12 @@ package com.stag22.journey;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.shaded.com.google.common.io.Files;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpHeaders;
 import com.github.javafaker.Faker;
@@ -12,6 +17,7 @@ import com.stag22.customer.CustomerDTO;
 import com.stag22.customer.CustomerRegistrationRequest;
 import com.stag22.customer.CustomerUpdateRequest;
 import com.stag22.customer.Gender;
+import com.stag22.storage.AvatarStorageUtil;
 
 import reactor.core.publisher.Mono;
 
@@ -19,6 +25,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -254,5 +261,85 @@ public class CustomerIntegrationTest {
  		assertThat(updatedCustomer)
  			.usingRecursiveComparison()
  			.isEqualTo(customer);
+	}
+	
+	@Test
+	void canUploadandDownloadProfilePicture() throws IOException {
+		//create registration request
+		Faker FAKER = new Faker();
+		String name = FAKER.name().fullName();
+		String email = FAKER.name().firstName() + UUID.randomUUID() + "@gmail.com";
+		int age = FAKER.random().nextInt(18, 90);
+		Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+ 		CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+ 				name, email, age, gender, "password");
+		
+		//send a post request
+ 		String jwtToken = postmann.post()
+ 			.uri("/api/v1/customers")
+ 			.accept(MediaType.APPLICATION_JSON)
+ 			.contentType(MediaType.APPLICATION_JSON)
+ 			.body(Mono.just(request), CustomerRegistrationRequest.class)
+ 			.exchange()
+ 			.expectStatus()
+ 			.isOk()
+ 			.returnResult(Void.class)
+ 			.getResponseHeaders()
+ 			.get(HttpHeaders.AUTHORIZATION)
+ 			.get(0);
+ 		
+		//get all customers
+ 		List<CustomerDTO> allCustomers = postmann.get()
+ 			.uri("/api/v1/customers")
+ 			.accept(MediaType.APPLICATION_JSON)
+ 			.header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+ 			.exchange()
+ 			.expectStatus()
+ 			.isOk()
+ 			.expectBodyList(new ParameterizedTypeReference<CustomerDTO>() {})
+ 			.returnResult()
+ 			.getResponseBody();
+		
+		//get customer by id
+ 		var id = allCustomers.stream()
+ 				.filter(c -> c.email().equals(email))
+ 				.map(CustomerDTO::id)
+ 				.findFirst()
+ 				.orElseThrow();
+ 		
+ 		//When
+ 		Resource image = new ClassPathResource("male.png");
+ 		MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+ 		bodyBuilder.part("file", image);
+ 		
+ 		//{customerId}/profile-image
+ 		//send a post request
+ 		postmann.post()
+ 			.uri("/api/v1/customers" +"/{customerId}/profile-image", id)
+ 			.body(BodyInserters.fromMultipartData(bodyBuilder.build()))
+ 			.header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+ 			.exchange()
+ 			.expectStatus()
+ 			.isOk();
+ 		
+ 		// Then the profile image shall be downloaded
+ 		byte[] downloadedImage = postmann.get()
+			.uri("/api/v1/customers" + "/{id}/profile-image", id)
+			.accept(MediaType.APPLICATION_JSON)
+			.header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody(byte[].class)
+			.returnResult()
+			.getResponseBody();
+ 		
+ 		byte[] actual = Files.toByteArray(image.getFile());
+ 		
+ 		assertThat(actual).isEqualTo(downloadedImage);
+ 		
+ 		// TearDown
+ 		AvatarStorageUtil.drop_avatars();
+ 		
 	}
 }
